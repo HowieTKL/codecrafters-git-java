04ef713b4edbf3b17ab8399549ed988e4b652515
HowieTKL <10776992+HowieTKL@users.noreply.github.com>
2025-03-06 08:20:05 -0500

    cat-file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.zip.InflaterInputStream;
 
 public class Main {
+  private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
     final String command = args[0];
 
@@ -21,6 +30,23 @@ public class Main {
           throw new RuntimeException(e);
         }
       }
+      case "cat-file" -> {
+        if (args.length < 2 && !"-p".equals(args[0])) {
+          return;
+        }
+        String blobSha = args[1];
+        String filePath = String.format(".git/objects/%s/%s", blobSha.substring(0, 2), blobSha.substring(2));
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new InflaterInputStream(new FileInputStream(filePath))))) {
+          String line = reader.readLine();
+          System.out.println(line.substring(line.indexOf("\0" + 1)));
+          while ((line = reader.readLine()) != null) {
+            System.out.print(line);
+          }
+        } catch (Exception e) {
+          LOG.error(e.getMessage(), e);
+        }
+      }
+
       default -> System.out.println("Unknown command: " + command);
     }
   }
